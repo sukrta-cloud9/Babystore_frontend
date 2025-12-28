de61657c5f05d9f4bedcd4e3e669b7d4afcaaa49
sukrta-cloud9 <sukrtaga01@gmail.com>
2025-12-28 13:38:13 +0530

    slight changes-packaage.json

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -17,7 +17,7 @@ export default function ProductDetails() {
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/products/${id}`)
+    fetch(`https://babystore-frontend.onrender.com/products/${id}`)
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, [id]);
