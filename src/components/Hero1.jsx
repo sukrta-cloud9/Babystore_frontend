9f7c6403c17c59e1a5672272f18445418756e7e9
sukrta-cloud9 <sukrtaga01@gmail.com>
2026-02-25 22:25:01 +0530

    fix3

--- src/components/Hero1.jsx
+++ src/components/Hero1.jsx
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 export default function Hero1() {
   return (
     <section className="hero-section">
-      <div className="hero-content">
+      <div className="hero-inner">
         <h1>Wrap Your Baby with Love</h1>
         <p>Made with care, crafted for your little one...</p>
-      </div>
-      <div className="content2 mt-3">
-      <p >Every product brings comfort, safety, and hugs your <br/>little one with warmth.</p>
+      
+      
+      <p className="tagline">
+        Every product brings comfort, safety, and hugs your <br/>little one with warmth.
+      </p>
       
 <Link to="/products" className="pastel-btn">Shop Now</Link>
 
